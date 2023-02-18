121b647e1b28d560812d7469d5d0aafd9137e917
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-18 04:12:09 +0300

    3-rd power

---
+++ Task_23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Input number:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Here are numbers from 1 to {0} to the third power:", n);
+
+for (int i=1; i<=n; i++)
+{
+    double num = Math.Pow(i, 3);
+
+    Console.Write("{0} ", num);
+}
